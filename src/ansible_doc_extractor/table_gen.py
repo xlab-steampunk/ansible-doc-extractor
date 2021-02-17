900132c9e92601f0f4247377daa8a3b255049454
Hunter Thompson <hthompson@a10networks.com>
2021-02-17 23:07:07 +0000

    Added indent handling and some toString

--- src/ansible_doc_extractor/table_gen.py
+++ src/ansible_doc_extractor/table_gen.py
@@ -14,7 +14,8 @@
 
 import enum
 
-CELL_TYPE = enum.Enum('CELL_TYPE', 'PARAM CHO_DEF COMMENT')
+CELL_TYPE = enum.Enum('CELL_TYPE', 'INDENT PARAM CHO_DEF COMMENT')
+
 
 class IncorrectCellTypeException(Exception):
 
@@ -33,13 +34,39 @@ class ListNode(object):
 class RowNode(ListNode):
     cells = []
 
+    def __str__(self, max_param_len, max_cho_def_len, max_comment_len):
+        row_str = "|"
+        indent_cnt = len(self.cells) - 3 # If we have extra cells then these are for suboption indenting
+        assert indent_cnt >= 0
+        for i in range(indent_cnt):
+            indent_cell_str = " {} |".format(self.cells[i])
+            row_str += indent_cell_str
+            max_param_len -= len(indent_cell_str)
+
+        param = str(self.cells[indent_cnt])
+        cho_def = str(self.cells[indent_cnt + 1])
+        comment = str(self.cells[indent_cnt + 2])
+
+        cell_padding = lambda x, y: x + " " * y
+        col1_str = cell_padding(param, abs(len(param) - max_param_len))
+        col2_str = cell_padding(cho_def, abs(len(cho_def) - max_cho_def_len))
+        col3_str = cell_padding(comment, abs(len(comment) - max_comment_len))
+
+        row_str += " {} | {} | {} |".format(col1_str, col2_str, col3_str)
+        return row_str
+
 
 class RowSpacerNode(ListNode):
     
-    def __init__(self, spacer_char, cell_cnt=0, next_node=None, prev_node=None):
+    def __init__(self, spacer_char, level=0, next_node=None, prev_node=None):
         self.spacer_char = spacer_char
-        self.cell_cnt = cell_cnt
+        self.level = level
         super().__init__(next_node, prev_node)
+
+    def __str__(self, max_param_len, max_cho_def_len, max_comment_len):
+        #fill = lambda x, y: x*y
+        return " "
+
 
 
 class Cell(object):
@@ -51,6 +78,9 @@ class Cell(object):
             raise IncorrectCellTypeException(cell_type)
         self.cell_type = cell_type
 
+    def __str__(self):
+        return str(self.content)
+
 
 class Table(object):
 
@@ -58,23 +88,19 @@ class Table(object):
         self.max_param_len = 0
         self.max_cho_def_len = 0
         self.max_comment_len = 0
+        self.max_level = 0
         self.head_node = None
         self.tail_node = None
 
-    def _build_row_cells(self, param, data):
-        cells = []
-        
-        if len(param) > self.max_param_len:
-            self.max_param_len = len(param)
-            cells.append(Cell(param, CELL_TYPE.PARAM))
+    def _build_row_cells(self, param, data, level):
 
-        description = ""
-        if data.get("description"):
-            # Description element is wrapped in a list block though it is just a single entry
-            description = data["description"][0]
-            if len(description) > self.max_comment_len:
-                self.max_comment_len = len(description)
-        cells.append(Cell(description, CELL_TYPE.COMMENT))
+        cells = []
+        for i in range(level):
+            cells.append(Cell(" ", CELL_TYPE.INDENT))
+        param_len = len(param) + level
+        if param_len > self.max_param_len:
+            self.max_param_len = param_len
+        cells.append(Cell(param, CELL_TYPE.PARAM))
 
         cho_def = ""
         if data.get("choices"):
@@ -83,44 +109,86 @@ class Table(object):
                 self.max_cho_def_len = len(cho_def)
         cells.append(Cell(cho_def, CELL_TYPE.CHO_DEF))
 
+        description = ""
+        if data.get("description"):
+            # Description element is wrapped in a list block though it is just a single entry
+            description = data["description"][0]
+            if len(description) > self.max_comment_len:
+                self.max_comment_len = len(description)
+        cells.append(Cell(description, CELL_TYPE.COMMENT))
         return cells
 
     def _build_row_str(self):
         pass
 
+    def _add_spacer(self, future_node, level=0):
+        row_spacer = RowSpacerNode("-", level)
+        future_node.next_node = row_spacer
+        row_spacer.prev_node = future_node
+
+        # Update level of upper spacer to allow for column creation
+        upper_spacer = row_spacer.prev_node.prev_node
+        assert type(upper_spacer) == type(row_spacer)
+        level_delta = row_spacer.level - upper_spacer.level
+        if level_delta > 0:
+            upper_spacer.level += level_delta
+        return row_spacer
+
     def _build_row_dll(self, head_node, data, level=0):
+        if level > self.max_level:
+            self.max_level = level
         future_node = RowNode()
         head_node.next_node = future_node
         future_node.prev_node = head_node
 
-        for k, v in data.items():
-            future_node.cells = self._build_row_cells(k, v)
-            if v.get('suboptions'):
-                tail_node = self._build_row_dll(future_node, v['suboptions'], level+1)
-                #TODO: Add spacer
-                #spacer = RowSpacerNode("=", 0)
-                future_node = RowNode()
-                tail_node.next_node = future_node
-                future_node.prev_node = tail_node
-            else:
-                #TODO: Add spacer
-                #spacer = RowSpacerNode("=", 0)
+        options = list(data.keys())
+        future_node.cells = self._build_row_cells(options[0], data[options[0]], level)
+        row_spacer = self._add_spacer(future_node, level)
 
-                temp_node = future_node
-                future_node = RowNode()
-                temp_node.next_node = future_node
-                future_node.prev_node = temp_node
-            future_node.next_node = None # Ensure that no next is set if at the end
-        return future_node
+        tail_node = None
+        for i in range(1, len(options)):
+            param = options[i]
+            param_val_dict = data[options[i]]
+
+            tail_node = None # Overwrite tail_node if it wasn't the last
+
+            future_node = RowNode()
+            future_node.prev_node = row_spacer
+            row_spacer.next_node = future_node
+            future_node.cells = self._build_row_cells( param, param_val_dict, level)
+            row_spacer = self._add_spacer(future_node)
+
+            if param_val_dict.get('suboptions'):
+                tail_node = self._build_row_dll(row_spacer, param_val_dict['suboptions'], level+1)
+                if tail_node:
+                    row_spacer = tail_node
+
+
+        if tail_node:
+            return tail_node
+        return row_spacer
 
     def build_table(self, data):
-        header_spacer = RowSpacerNode("=", 3)
-        self.head_node = RowNode()
-        self.head_node.prev_node = header_spacer
-        header_spacer.next_node = self.head_node
-        row_spacer = RowSpacerNode('-', 3)
-        row_spacer.prev_node = self.head_node
-        self.head_node.next_node = row_spacer
-        # TODO: self.head_node.cells = 
+                # TODO: self.head_node.cells = 
+        row_spacer = RowSpacerNode("-", 3)
+        head_node = RowNode()
+        head_node.prev_node = row_spacer
+        row_spacer.next_node = head_node
+        head_dict = {'description': ['Comment'],
+                     'choices': 'Choices/Defaults'}
+        head_node.cells = self._build_row_cells('Parameters', head_dict, level=0)
+
+        head_spacer = RowSpacerNode('=', 3)
+        head_spacer.prev_node = head_node
+        head_node.next_node = head_spacer
+
         if data:
-            self._build_row_dll(self.head_node, data)
+            self._build_row_dll(head_spacer, data)
+
+        table = []
+        while head_node != None:
+            table.append(head_node.__str__(self.max_param_len, self.max_cho_def_len, self.max_comment_len))
+            head_node = head_node.next_node
+
+        return table
+
